88860ecfdeb652ffe25a465a70c7f474c1252901
Jenny Martin <jennymartin@Jennys-MBP.lan1>
2019-08-02 11:30:59 -0500

    first commit

---
+++ src/todosData.js
@@ -0,0 +1,34 @@
+const todosData = [
+    {
+        id: 1,
+        text: "take last assessment",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "finish react project",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "have fun woohoo weekend",
+        completed: false
+    }, 
+    {
+        id: 4, 
+        text: "do the dishes", 
+        completed: true
+    },
+    {
+        id: 5, 
+        text: "do the laundry", 
+        completed: true
+    },
+    {
+        id: 6, 
+        text: "take out the trash", 
+        completed: true
+    }
+]
+
+export default todosData;
